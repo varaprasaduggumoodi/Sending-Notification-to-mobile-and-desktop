a4695c0c418d8c684bc7e09b529e141dfe801b1d
varaprasaduggumoodi <122374448+varaprasaduggumoodi@users.noreply.github.com>
2023-01-10 19:49:52 +0530

    Add files via upload

---
+++ notification.py
@@ -0,0 +1,8 @@
+import requests
+requests.post('https://api.mynotifier.app', {
+    "apiKey": '1b932bed-862c-4ade-99e7-a2e1d16db857',
+    "message": "Python internship project!",
+    "description": "Task1:Sending notification through python for mobile is successfully completed",
+    "type": "success", 
+})
+
