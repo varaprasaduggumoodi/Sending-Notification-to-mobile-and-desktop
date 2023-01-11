259350e02efa321d36e3fe3c93d6cbefd7f6556d
varaprasaduggumoodi <122374448+varaprasaduggumoodi@users.noreply.github.com>
2023-01-11 22:39:20 +0530

    Update notification.py

--- notification.py
+++ notification.py
@@ -2,7 +2,9 @@ import requests
 from plyer import notification
 import time
 time.sleep(10)
+#below code sends the notification to the same desktop where the code is running
 notification.notify(title='Notifications for the Desktop',message=input("Enter the message : "),app_icon='image.ico',timeout=20,)
+#below code sends the notification to the mobile device which contains the app myNotifier.
 requests.post('https://api.mynotifier.app', {
     "apiKey": ' #provide the api key here ',
     "message": "Python internship project!",
