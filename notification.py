6d4eeb48fc852962f9a2e4c80825989cb2333df0
varaprasaduggumoodi <122374448+varaprasaduggumoodi@users.noreply.github.com>
2023-01-11 22:36:53 +0530

    Update notification.py

--- notification.py
+++ notification.py
@@ -1,6 +1,10 @@
 import requests
+from plyer import notification
+import time
+time.sleep(10)
+notification.notify(title='Notifications for the Desktop',message=input("Enter the message : "),app_icon='image.ico',timeout=20,)
 requests.post('https://api.mynotifier.app', {
-    "apiKey": '1b932bed-862c-4ade-99e7-a2e1d16db857',
+    "apiKey": ' #provide the api key here ',
     "message": "Python internship project!",
     "description": "Task1:Sending notification through python for mobile is successfully completed",
     "type": "success", 
